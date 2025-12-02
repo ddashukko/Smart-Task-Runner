63763887fe0c7ebd31cb8f0ba93c81c2f280471c
ddashukko <dasha.kerroll@gmail.com>
2025-12-02 23:28:19 +0200

    Optimize execution logic: decouple C1 from B1 to minimize runtime

--- Smart Task Runner/Smart Task Runner.cpp
+++ Smart Task Runner/Smart Task Runner.cpp
@@ -1,37 +1,44 @@
-//variant 12
 #include <iostream>
 #include <string>
 #include <future>
 #include <chrono>
 #include <thread>
 #include <syncstream>
+#include <vector>
 
 using namespace std;
 
 void compute(const string& name, int sec)
 {
     this_thread::sleep_for(chrono::seconds(sec));
-    osyncstream(cout) << name << "\n";            
+    osyncstream(cout) << "[" << this_thread::get_id() << "] "
+        << name << " completed (" << sec << " sec)\n";
 }
 
 void work()
 {
     using namespace chrono;
 
+    cout << "Main thread ID: " << this_thread::get_id() << "\n";
+    cout << "Starting work...\n";
+
     auto start = steady_clock::now();
 
     auto future_branch1 = async(launch::async, []()
         {
-            compute("A1", 7);      
-            compute("B1", 7);      
-            compute("C1", 1);      
+            compute("A1", 7);
+
+            auto f_c1 = async(launch::async, []() { compute("C1", 1); });
+            compute("B1", 7);
+
+            f_c1.get();
         });
 
     auto future_branch2 = async(launch::async, []()
         {
-            compute("A2", 1);      
-            compute("B2", 1);      
-            compute("C2", 1);      
+            compute("A2", 1);
+            compute("B2", 1);
+            compute("C2", 1);
         });
 
     future_branch1.get();
@@ -42,7 +49,7 @@ void work()
     auto end = steady_clock::now();
     auto total = duration_cast<seconds>(end - start).count();
 
-    cout << "Time: " << total << " seconds\n";
+    cout << "Total Time: " << total << " seconds\n";
     cout << "Work is done!\n";
 }
 
